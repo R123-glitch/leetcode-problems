fec3b795afcedd31194cfb9bdaec0c49724b2a86
R123-glitch <152021391+R123-glitch@users.noreply.github.com>
2025-05-31 16:22:33 +0530

    Time: 2 ms (69.73%), Space: 7.7 MB (68.64%) - LeetHub

---
+++ 1630-count-odd-numbers-in-an-interval-range/1630-count-odd-numbers-in-an-interval-range.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int count;
+        count=(((high+1)/2)-(low/2));
+        return count;
+        
+        
+    
+    }
+};

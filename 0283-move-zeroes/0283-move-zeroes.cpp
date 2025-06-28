7a16e21a5590770c11a1a7479a748a251c6360e0
R123-glitch <152021391+R123-glitch@users.noreply.github.com>
2025-06-28 13:02:38 +0530

    Time: 0 ms (100%), Space: 23.9 MB (50.78%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0;
-        // move all the nonzero elements advance
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                nums[j++] = nums[i];
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
             }
         }
-        for (;j < nums.size(); j++) {
-            nums[j] = 0;
-        }
+        
     }
 };

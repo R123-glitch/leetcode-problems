6accc04f3f8125cb28aa862c21fa3a8bec31888e
R123-glitch <152021391+R123-glitch@users.noreply.github.com>
2025-07-13 16:56:54 +0530

    Time: 0 ms (100%), Space: 97.4 MB (25.78%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit=0;
-        int buy=prices[0];
-        for(int i=0;i<prices.size();i++){
-            if(prices[i]<buy){
-                buy=prices[i];
-            }
-            else if(prices[i]-buy>profit){
-                profit=prices[i]-buy;
-            }
-            
+       int n=prices.size();
+       int profit=0;
+       int min=prices[0];
+       for(int i=1;i<n;i++){
+        
+        if(prices[i]<min){
+        min=prices[i];
         }
-        return profit;
+        else{
+            profit=max(prices[i]-min,profit);
+        }      
+        
+        
+       }
+       return profit;
     }
 };

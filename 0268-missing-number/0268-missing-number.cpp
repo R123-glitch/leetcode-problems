6e625f6b393311b22a42a1910e1a44412f5287f6
R123-glitch <152021391+R123-glitch@users.noreply.github.com>
2025-07-13 10:47:26 +0530

    Time: 0 ms (100%), Space: 21.8 MB (38.74%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int N=nums.size();
+        int sum = (N * (N + 1)) / 2;
+
+
+     //Summation of all array elements:
+     int s2 = 0;
+     for (int i = 0; i < N; i++) {
+        s2 += nums[i];
+    }
+
+    int missing = sum - s2;
+    return missing;
+        
+    }
+};
